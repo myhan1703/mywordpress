bc835264cb0b92996bc3dbf861ed787e4c340ea6
myhan1703 <myhannguyenhuynh1703@gmail.com>
2021-10-01 23:34:33 +0700

    edit lan 5

--- wp-login.php
+++ wp-login.php
@@ -90,7 +90,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wp-admin\/admin-ajax.php"}};
 <script type="text/javascript" src="https://myhan1703.github.io/mywordpress/wp-includes/js/wp-util.min.js?ver=5.8.1" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /*  */
-var userProfileL10n = {"user_id":"0","nonce":"8c92ebce0c"};
+var userProfileL10n = {"user_id":"0","nonce":"2bd6ea9740"};
 /*  */
 </script>
 <script type="text/javascript" id="user-profile-js-translations">
